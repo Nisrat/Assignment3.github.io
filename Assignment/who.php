8d39d5433435e53728cd5277c984d50da1fe47b9
Nisrat85 <jnisrat61@gmail.com>
2020-12-01 20:05:34 +0600

    Assignment-3

---
+++ Assignment/who.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<Title>Online Car Management </Title>
+</head>
+<body id="home">
+	<center><h1 style="font-size: 36px"> Online Car Management </h1></center>
+	<center><h4 style="font-size: 28px"> Are you user or Admin ? <h4></center>
+<center>	<a href="index.php"><input type="submit" value="User" style="font-size: 24px"/> </a>
+	
+	<a href="admin.php"><input type="submit" value="Admin" style="font-size: 24px"/> </a>
+	</center>
+	<br>
+</body>
+</html>
